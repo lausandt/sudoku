637053944fd0aa790fad3e1883d4af585607ae39
lausandt <laurens.sandt@protonmail.com>
2023-06-08 11:12:00 +0200

    added description of the project

---
+++ sudoku.py
@@ -0,0 +1,11 @@
+""" 
+We are going to create a sudoku solver.
+In order to do this we should consider the data of which a sudoku puzzle exists
+A puzzle has:
+9 rows
+9 columns
+9 squares 
+81 units 
+The goal of sudoku is the have permutations of 1..9 in all rows, columns and squares
+ 
+"""
